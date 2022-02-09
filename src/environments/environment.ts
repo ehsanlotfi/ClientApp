b5befe742aefddd01df6a7a3d05e9afd75a7761e
ehsan lotfi <lotfiniamail@gmail.com>
2022-02-09 23:17:31 +0330

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,5 @@
 export const environment = {
   production: false,
   origin: 'https://origin.ir/api/',
-  originKpi: 'https://origin.ir/kpi/',
-  firebase: {
-    apiKey: "AIzaSyB0hWMNZR1Bg0Gl6f98qNAspcW32BbdEZ0",
-    authDomain: "pwa-message-b93aa.firebaseapp.com",
-    databaseURL: "https://pwa-message-b93aa.firebaseio.com",
-    projectId: "pwa-message-b93aa",
-    storageBucket: "pwa-message-b93aa.appspot.com",
-    messagingSenderId: "779115212521",
-    appId: "1:779115212521:web:e3b82ae5556ad531e1ca78",
-    measurementId: "G-G9Z2Q922NV"
-  },
   version: '0.0.01'
 };
