0fc43a94b8483b2602b8a10317257eb466c1125a
ehsan lotfi <lotfiniamail@gmail.com>
2022-07-12 10:51:41 +0430

    Update moment.pipe.ts
    cahnge jalali pipe

--- src/app/share/pipes/moment.pipe.ts
+++ src/app/share/pipes/moment.pipe.ts
@@ -5,17 +5,12 @@ import * as moment from 'jalali-moment';
   name: 'jalali'
 })
 export class JalaliPipe implements PipeTransform {
-  transform(value: any, time?: string, args?: any): any {
+  transform(value: any, format?: string, args?: any): any {
 
-    if(time){
-      let MomentDate = moment(value, 'YYYY/MM/DD '+time);
-      return MomentDate.locale('fa').format('YYYY/M/D [,]'+time);
-    } else {
-      if (!value) return value;
-      if (value.length < 9) return value;
-      if (!moment(value).isValid()) return value;
-      return moment.from(value, 'en', 'YYYY-MM-DDTHH:mm:ss').locale('fa').format('YYYY/MM/DD HH:MM')
-    }
+    if (!value) return value;
+    if (value.length < 9) return value;
+    if (!moment(value).isValid()) return value;
+    return moment.from(value, 'en', 'YYYY-MM-DDTHH:mm:ss').locale('fa').format(format ? format : 'YYYY/MM/DD HH:MM')
     
   }
 }
